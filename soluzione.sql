cd4c4311c9472161323b02d76d38df7b0c1e1713
Lex95 <andrea.lega1995@gmail.com>
2021-06-09 16:10:27 +0200

    risoluzione esercizio

--- soluzione.sql
+++ soluzione.sql
@@ -9,3 +9,35 @@ FROM `ospiti`
 WHERE YEAR(`date_of_birth`) > "1988";
 
 -- es3
+SELECT *
+FROM `ospiti`
+WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > "20";
+
+-- es4
+SELECT *
+FROM `ospiti`
+WHERE LEFT(`name`, 1) = "D";
+
+-- es5
+SELECT SUM(`price`)
+FROM `pagamenti`
+WHERE `status` = "accepted";
+
+-- es6
+SELECT MAX(`price`)
+FROM `pagamenti`;
+
+-- es7
+SELECT *
+FROM `ospiti`
+WHERE `document_type` = "Driver License"
+    AND YEAR(`date_of_birth`) = "1975";
+
+-- es8
+SELECT *
+FROM `paganti`
+WHERE `ospite_id` <> "NULL";
+
+-- es9
+SELECT SUM(`beds`)
+FROM `stanze`;
