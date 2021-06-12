0d73b19b1948d2f6ebe2fbcc6f9d12b6d5b3079e
Lex95 <andrea.lega1995@gmail.com>
2021-06-12 15:57:19 +0200

    risoluzione ultimi esercizi sul join

--- soluzioneJoin.sql
+++ soluzioneJoin.sql
@@ -12,3 +12,32 @@ INNER JOIN `prenotazioni_has_ospiti`
 	ON `ospiti`.`id` = `prenotazioni_has_ospiti`.`ospite_id`;
 
 -- es3
+SELECT `pagamenti`.`prenotazione_id`, `paganti`.`name`, `paganti`.`lastname`, `pagamenti`.`price`
+FROM `pagamenti`
+INNER JOIN `paganti`
+	ON `pagamenti`.`pagante_id` = `paganti`.`id`;
+
+-- es4
+SELECT SUM(`pagamenti`.`price`)
+FROM `stanze`
+LEFT JOIN `prenotazioni`
+	ON `stanze`.`id` = `prenotazioni`.`stanza_id`
+LEFT JOIN `pagamenti`
+	ON `prenotazioni`.`id` = `pagamenti`.`prenotazione_id`
+WHERE `stanze`.`floor` = 1;
+
+-- es5
+SELECT `paganti`.`address`
+FROM `prenotazioni`
+LEFT JOIN `pagamenti`
+	ON `prenotazioni`.`id` = `pagamenti`.`prenotazione_id`
+LEFT JOIN `paganti`
+	ON `pagamenti`.`pagante_id` = `paganti`.`id`
+WHERE `prenotazioni`.`id` = 7;
+
+-- es6
+SELECT `stanze`.*
+FROM `stanze`
+LEFT JOIN `prenotazioni`
+	ON `stanze`.`id` = `prenotazioni`.`stanza_id`
+WHERE `prenotazioni`.`id` IS NULL;
